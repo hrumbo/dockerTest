8a7d8db08a030a6c06a29fc6b6b36cf8a80f1d82
Hernan Rumbo <hernan.rumbo@endava.com>
2023-09-11 01:08:26 -0300

    Modifying Jenkinsfile

--- tests/src/axios.ts
+++ tests/src/axios.ts
@@ -1,8 +1,8 @@
 import axios, {AxiosRequestConfig} from "axios";
 
-//const baseURL= "http://localhost:3000"; //Your baseURL
+const baseURL= "http://localhost:3000"; //Your baseURL
 
-const baseURL= "http://172.17.0.2:3000"; //Your baseURL
+//const baseURL= "http://172.17.0.2:3000"; //Your baseURL
 
 //const baseURL= `http://${process.env.CONTAINER_IP || 'localhost'}:3000`; //Your baseURL
 
