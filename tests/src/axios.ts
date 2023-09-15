c6b5cce555fb835a9ff2e865ee3cf7bf222a2b80
Hernan Rumbo <hernan.rumbo@endava.com>
2023-09-15 04:24:19 -0300

    Modifying pipeline files

--- tests/src/axios.ts
+++ tests/src/axios.ts
@@ -2,9 +2,7 @@ import axios, {AxiosRequestConfig} from "axios";
 
 //const baseURL= "http://localhost:3000"; //Your baseURL
 
-//const baseURL= `http://${process.env.CONTAINER_IP || 'localhost:3000'}`; //Your baseURL
-
-const baseURL= "http://172.17.0.3:3000"; //Your baseURL
+const baseURL= `http://${process.env.CONTAINER_IP || 'localhost'}:3000`; //Your baseURL
 
 console.log("BASE URL: " + baseURL);
 
