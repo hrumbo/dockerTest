378d41d21c3dad277821828b8dcf0136f0371102
Hernan Rumbo <hernan.rumbo@endava.com>
2023-09-15 05:27:12 -0300

    Modifying pipeline files

--- tests/src/axios.ts
+++ tests/src/axios.ts
@@ -5,7 +5,7 @@ import axios, {AxiosRequestConfig} from "axios";
 //const baseURL= `http://${process.env.CONTAINER_IP || 'localhost'}:3000`; //Your baseURL
 
 const baseURL = process.env.DOMAIN_URL
-  ? `http://${process.env.DOMAIN_URL}` // Use domain_url if it exists
+  ? `${process.env.DOMAIN_URL}` // Use domain_url if it exists
   : `http://${process.env.CONTAINER_IP || 'localhost'}:3000`; // Fallback to CONTAINER_IP or localhost
 
 
